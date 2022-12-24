fbd5bd30bcd159030c82cb82233397f6ef0beb44
Romain Gréaume <r.greaume@beapp.fr>
2022-12-24 19:32:06 +0100

    feat: create spacers

---
+++ lib/common/spacers/spacers.dart
@@ -0,0 +1,20 @@
+/// 4.0
+const double spaceXXS = 4.0;
+
+/// 8.0
+const double spaceXS = 8.0;
+
+/// 12.0
+const double spaceS = 12.0;
+
+/// 16.0
+const double spaceM = 16.0;
+
+/// 24.0
+const double spaceL = 24.0;
+
+/// 32.0
+const double spaceXL = 32.0;
+
+/// 48.0
+const double spaceXXL = 48.0;

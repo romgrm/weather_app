72dd4f6aeb6c9e48b05c0c94c809ebd6bb5ca7eb
Romain Gréaume <r.greaume@beapp.fr>
2022-12-25 19:15:41 +0100

    feat: add safeArea to authScreen

--- lib/PresentationLayer/screens/authentication.screen.dart
+++ lib/PresentationLayer/screens/authentication.screen.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:weather_app/common/widgets/logo.dart';
-import 'package:weather_app/localize/localize.dart';
-import 'package:weather_app/presentationLayer/bloc/authentication.cubit.dart';
-import 'package:weather_app/theme/colors.cubit.dart';
-import '../widgets/text_input.widget.dart';
 
 import '../../common/spacers/spacers.dart';
+import '../../common/widgets/logo.dart';
+import '../../localize/localize.dart';
+import '../../theme/colors.cubit.dart';
+import '../../utils/snackbar.utils.dart';
+import '../bloc/authentication.cubit.dart';
+import '../widgets/text_input.widget.dart';
 
 class AuthenticationScreen extends StatefulWidget {
   const AuthenticationScreen({super.key});
@@ -22,86 +23,97 @@ class _AuthenticationScreenState extends State<AuthenticationScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return BlocConsumer<AuthenticationCubit, AuthenticationState>(
-      listener: (context, state) {
-        state.whenOrNull(
-          loading: () {
-            print("loading");
-          },
-        );
-      },
-      builder: (context, state) {
-        return state.maybeWhen(
-          onSuccess: (user) => Center(
-            child: Text(
-              "OnSuccess",
-              style: Theme.of(context).textTheme.titleLarge?.copyWith(fontSize: 40, color: context.read<ColorsCubit>().state.getSecondary()),
-            ),
-          ),
-          orElse: () {
-            return ListView(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: spaceXL, vertical: spaceM),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      const Padding(
-                        padding: EdgeInsets.only(bottom: spaceXXL),
-                        child: LogoWidget(),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(top: spaceXXL),
-                        child: Form(
-                            key: _formKey,
-                            child: Column(
-                              children: [
-                                TextInputWidget(
-                                    hintText: "authenticationEmailTextInputLabel",
-                                    isObscure: false,
-                                    textControllerCallback: ((value) {
-                                      setState(() {
-                                        email = value;
-                                      });
-                                    })),
-                                TextInputWidget(
-                                    hintText: "authenticationPasswordTextInputLabel",
-                                    isObscure: true,
-                                    textControllerCallback: ((value) {
-                                      setState(() {
-                                        password = value;
-                                      });
-                                    })),
-                                Padding(
-                                  padding: const EdgeInsets.only(top: spaceM),
-                                  child: ElevatedButton(
-                                      onPressed: (email.isNotEmpty && password.isNotEmpty)
-                                          ? () {
-                                              if (_formKey.currentState!.validate()) {
-                                                context.read<AuthenticationCubit>().login(email, password);
-                                                FocusManager.instance.primaryFocus?.unfocus();
-                                              }
+    return Scaffold(
+      resizeToAvoidBottomInset: true,
+      body: SafeArea(
+        child: ListView(
+          children: [
+            BlocConsumer<AuthenticationCubit, AuthenticationState>(
+              listener: (context, state) {
+                state.whenOrNull(
+                  loading: () {
+                    showDialog(
+                        context: context,
+                        barrierDismissible: false,
+                        builder: (BuildContext context) {
+                          return const Center(
+                            child: CircularProgressIndicator(),
+                          );
+                        });
+                  },
+                  onError: (errorMessage) {
+                    WidgetsBinding.instance.addPostFrameCallback((_) {
+                      Navigator.of(context).pop();
+                      SnackBarUtils.showSnackBar(context, message: Text(errorMessage));
+                    });
+                  },
+                  onSuccess: (user) {
+                    Navigator.of(context).pop();
+                    Navigator.pushNamed(context, "/home");
+                  },
+                );
+              },
+              builder: (context, state) {
+                return state.maybeWhen(
+                  orElse: () {
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: spaceXL, vertical: spaceM),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          const Padding(
+                            padding: EdgeInsets.only(bottom: spaceXXL),
+                            child: LogoWidget(),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: spaceXXL),
+                            child: Form(
+                                key: _formKey,
+                                child: Column(
+                                  children: [
+                                    TextInputWidget(
+                                        hintText: "authenticationEmailTextInputLabel",
+                                        isObscure: false,
+                                        textControllerCallback: ((value) {
+                                          setState(() {
+                                            email = value;
+                                          });
+                                        })),
+                                    TextInputWidget(
+                                        hintText: "authenticationPasswordTextInputLabel",
+                                        isObscure: true,
+                                        textControllerCallback: ((value) {
+                                          setState(() {
+                                            password = value;
+                                          });
+                                        })),
+                                    Padding(
+                                      padding: const EdgeInsets.only(top: spaceM),
+                                      child: ElevatedButton(
+                                          onPressed: () {
+                                            if (_formKey.currentState!.validate()) {
+                                              context.read<AuthenticationCubit>().login(email, password);
+                                              FocusManager.instance.primaryFocus?.unfocus();
                                             }
-                                          : null,
-                                      child: Text(
-                                        getLocalize("authValidateButtonLabel"),
-                                        style: Theme.of(context).textTheme.titleSmall?.copyWith(fontSize: 20, color: context.read<ColorsCubit>().state.getPrimary()),
-                                      )),
-                                )
-                              ],
-                            )),
+                                          },
+                                          child: Text(
+                                            getLocalize("authValidateButtonLabel"),
+                                            style: Theme.of(context).textTheme.titleSmall?.copyWith(fontSize: 20, color: context.read<ColorsCubit>().state.getPrimary()),
+                                          )),
+                                    )
+                                  ],
+                                )),
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
-                ),
-              ],
-            );
-          },
-          onError: (errorMessage) {
-            return Text(errorMessage);
-          },
-        );
-      },
+                    );
+                  },
+                );
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }

da8b6655e97e79976744884737f160c1f03aa61e
Romain Gréaume <r.greaume@beapp.fr>
2022-12-25 17:06:20 +0100

    feat: create authRepository

---
+++ lib/dataLayer/repositories/authentication.repository.dart
@@ -0,0 +1,20 @@
+import 'package:weather_app/dataLayer/datasources/authentication_impl.data.dart';
+import 'package:weather_app/dataLayer/dto/user.dto.dart';
+import 'package:weather_app/domainLayer/user.entity.dart';
+
+import '../../core/di/service_locator.dart';
+import '../datasources/authentication.data.dart';
+
+class AuthenticationRepository {
+  final _provider = getIt<AuthenticationDataSource>();
+
+  Future<UserEntity?> login(String email, String password) async {
+    final UserDto? userDto = await _provider.login(email, password);
+
+    if (userDto != null) {
+      return userDto.toEntity();
+    } else {
+      return null;
+    }
+  }
+}

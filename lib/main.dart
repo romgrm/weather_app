1fea88017ec57098b89f2412abbbfa368bae2833
Romain Gréaume <r.greaume@beapp.fr>
2022-12-25 14:20:14 +0100

    chore: add Bloc library + create Colors Cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather_app/theme/colors.cubit.dart';
 
 import 'Theme/app.theme.dart';
 import 'presentationLayer/screens/authentication.screen.dart';
 
 void main() {
-  runApp(const App());
+  runApp(const AppProviders());
+}
+
+class AppProviders extends StatefulWidget {
+  const AppProviders({super.key});
+
+  @override
+  State<AppProviders> createState() => _AppProvidersState();
+}
+
+class _AppProvidersState extends State<AppProviders> {
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => ColorsCubit()),
+      ],
+      child: const App(),
+    );
+  }
 }
 
 class App extends StatefulWidget {
@@ -14,7 +37,36 @@ class App extends StatefulWidget {
   State<App> createState() => _AppState();
 }
 
-class _AppState extends State<App> {
+class _AppState extends State<App> with WidgetsBindingObserver {
+  late Brightness _brightness;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+    _updateTheme();
+  }
+
+  @override
+  void didChangePlatformBrightness() {
+    _updateTheme();
+    super.didChangePlatformBrightness();
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  void _updateTheme() {
+    _brightness = SchedulerBinding.instance.window.platformBrightness;
+
+    context.read<ColorsCubit>().update(_brightness);
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(

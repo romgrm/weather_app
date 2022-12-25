bda87babb6da2ba7526dfe97c358eb6f3ae77fea
Romain Gréaume <r.greaume@beapp.fr>
2022-12-25 13:40:28 +0100

    feat: impl form with TextInputWidget on authScreen

--- lib/localize/localize.dart
+++ lib/localize/localize.dart
@@ -6,4 +6,6 @@ Map<String, String> data = {
   // Authentication
   "authenticationEmailTextInputLabel": "Email",
   "authenticationPasswordTextInputLabel": "Mot de passe",
+  "authMailValidatorText": "Entrez un email valide",
+  "authPasswordValidatorText": "Entrez un mot de passe valide",
 };

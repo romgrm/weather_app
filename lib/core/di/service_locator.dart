6ca28afd515a88046d5569e30d2f44919d15ba62
Romain Gréaume <r.greaume@beapp.fr>
2022-12-25 17:07:31 +0100

    chore: update serviceLocator with datasources

--- lib/core/di/service_locator.dart
+++ lib/core/di/service_locator.dart
@@ -1,5 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
+import 'package:weather_app/dataLayer/datasources/authentication.data.dart';
+import 'package:weather_app/dataLayer/datasources/authentication_impl.data.dart';
 
 import '../rest/restManager.dart';
 
@@ -11,4 +13,7 @@ void setUpGetIt() {
 
   // Authentication & REST
   getIt.registerLazySingleton<RestManager>(() => RestManager(dio: getIt<Dio>()));
+
+  // Datasources
+  getIt.registerFactory<AuthenticationDataSource>(() => AuthenticationDataSourceImpl(getIt<RestManager>()));
 }

c0c0d497007d661502ceadc905b46565a55f497e
Romain Gréaume <r.greaume@beapp.fr>
2022-12-26 18:11:58 +0100

    feat: impl call API for fetch weatherDays

--- lib/dataLayer/datasources/weather_impl.data.dart
+++ lib/dataLayer/datasources/weather_impl.data.dart
@@ -1,9 +1,22 @@
+import 'package:weather_app/core/rest/restManager.dart';
 import 'package:weather_app/dataLayer/datasources/weather.data.dart';
 import 'package:weather_app/dataLayer/dto/weather_day.dto.dart';
 
 class WeatherDataSourceImpl implements WeatherDataSource {
+  RestManager restManager;
+  WeatherDataSourceImpl(
+    this.restManager,
+  );
   @override
   Future<List<WeatherDayDto>?> getWeatherForFiveDays() async {
-    return await [WeatherDayDto()];
+    List<WeatherDayDto> weatherDays = [];
+    final params = {"id": 2968815, "appid": "b212ee35da614193703096d6d6644c3c", "units": "metric", "lang": "fr"};
+
+    final json = await restManager.get(path: "https://api.openweathermap.org/data/2.5/forecast?", parameters: params);
+    weatherDays = List<WeatherDayDto>.from(json['list'].map((json) {
+      return WeatherDayDto.fromJson(json);
+    }));
+
+    return weatherDays;
   }
 }

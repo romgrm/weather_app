0e018eee081f1298d5a597d4170ecd8e03edf892
Romain Gréaume <r.greaume@beapp.fr>
2022-12-26 16:55:55 +0100

    test: should return list of WeatherDay passed

--- lib/dataLayer/datasources/weather_impl.data.dart
+++ lib/dataLayer/datasources/weather_impl.data.dart
@@ -3,8 +3,7 @@ import 'package:weather_app/dataLayer/dto/weather_day.dto.dart';
 
 class WeatherDataSourceImpl implements WeatherDataSource {
   @override
-  Future<List<WeatherDayDto>?> getWeatherForFiveDays() {
-    // TODO: implement getWeatherForFiveDays
-    throw UnimplementedError();
+  Future<List<WeatherDayDto>?> getWeatherForFiveDays() async {
+    return await [WeatherDayDto()];
   }
 }

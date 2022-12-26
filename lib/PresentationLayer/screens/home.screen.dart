1cae50a98e36984c18a0502f6ca2c63d8bf1ece5
Romain Gréaume <r.greaume@beapp.fr>
2022-12-26 18:13:57 +0100

    feat: impl WeatherCubit in UI for fetching/displaying data (WIP)

--- lib/PresentationLayer/screens/home.screen.dart
+++ lib/PresentationLayer/screens/home.screen.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../core/di/service_locator.dart';
+import '../../dataLayer/datasources/weather.data.dart';
+import '../bloc/weather.cubit.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -9,10 +14,43 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   @override
+  void initState() {
+    super.initState();
+    context.read<WeatherCubit>().getWeatherForFiveDays();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Center(child: Text("Home", style: TextStyle(color: Colors.red))),
+      body: BlocBuilder<WeatherCubit, WeatherState>(
+        builder: (context, state) {
+          return state.when(
+            onSuccess: (weatherDays) {
+              // return Center(
+              //   child: Text("success", style: Theme.of(context).textTheme.titleMedium?.copyWith(color: Colors.red)),
+              // );
+              return ListView.builder(
+                itemBuilder: ((context, index) {
+                  return Text(
+                    "${weatherDays[index].main?.temp ?? ""}",
+                    style: Theme.of(context).textTheme.titleMedium?.copyWith(color: Colors.red),
+                  );
+                }),
+                itemCount: weatherDays.length,
+              );
+            },
+            onError: (errorMessage) => Text("error", style: Theme.of(context).textTheme.titleMedium?.copyWith(color: Colors.red)),
+            loading: () => Center(child: Text("loading", style: Theme.of(context).textTheme.titleMedium?.copyWith(color: Colors.red))),
+          );
+          // return Container(
+          //   child: Center(
+          //       child: ElevatedButton(
+          //           onPressed: () {
+          //             // provider.getWeatherForFiveDays();
+          //           },
+          //           child: Text("test"))),
+          // );
+        },
       ),
     );
   }
